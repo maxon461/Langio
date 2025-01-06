5ad7db86e948abe0df21b8fd70ad1a6278d560d9
JedrzejSobkow <sobkowjedrzej@gmail.com>
2025-01-06 22:34:47 +0100

    Connected dataUser with levelMenu. Changed hint in ExamTranslateScreen (before 'la manzana' was represented by '_ _ _ _ _ _ _ _ _ _', but now by '_ _   _ _ _ _ _ _ _')

--- app/src/main/java/com/example/langio/useful/useful.kt
+++ app/src/main/java/com/example/langio/useful/useful.kt
@@ -75,10 +75,7 @@ fun CustomBottomNavigationBar(selectedTab: String, modifier: Modifier = Modifier
             label = "Map",
             imageResId = R.drawable.cow,
             isSelected = selectedTab == "map",
-            onClick = {
-                GameController.instance.unlockLevel()
-                GameController.instance.changeScreen(GameController.Screen.MAP)
-                      },
+            onClick = { GameController.instance.changeScreen(GameController.Screen.MAP) },
             modifier = Modifier.weight(1f)
         )
 
